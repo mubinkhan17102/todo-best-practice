c2b974eb2897cd6050aa71fbe03ac8c601333981
mubinkhan17102 <mobin17102@gmail.com>
2023-02-21 16:52:13 +0600

    Add todo container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import { TodoContainer } from "./containers/todoContainer/TodoContainer";
 import './App.scss';
 
 function App() {
   return (
     <>
       <Header/>
+      <TodoContainer/>
     </>
   );
 }
---
+++ src/containers/todoContainer/TodoContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './TodoContainer.modules.scss'
+
+export const TodoContainer = () => {
+  return (
+    <div>TodoContainer</div>
+  )
+}
